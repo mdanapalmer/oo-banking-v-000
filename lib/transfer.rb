f8e635650c153d8c7765fb5ec32f236f2949008b
Dana Palmer <roiai4@hotmail.com>
2018-10-25 04:10:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,5 +35,8 @@ attr_accessor :sender, :receiver, :status, :amount, :balance
     else
       @status = 'rejected'
       "transaction rejected. Please check your account balance."
+    end
+  end
+  
 
 end
