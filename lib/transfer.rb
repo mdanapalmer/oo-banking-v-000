3acd2bf33f3e17fa5964603e9e04d4fc6418b73c
Dana Palmer <roiai4@hotmail.com>
2018-10-25 03:13:37 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,18 +18,14 @@ attr_accessor :sender, :receiver, :status, :amount, :balance
 
   def execute_transaction
     while valid?
-      counter = 0
       @sender.balance -= amount
       @receiver.balance += amount
       @status = 'complete'
-      counter +-1
-      counter == 1
-      break
     else
       "Transaction rejected. Please check your account balance."
+    end
   end
-end
-end
+  end
 end
 
 end
