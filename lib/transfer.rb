403455f25b5a9a48102065d19d858f981eb58614
Dana Palmer <roiai4@hotmail.com>
2018-10-25 03:02:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,8 @@ attr_accessor :sender, :receiver, :status, :amount, :balance
 
   def execute_transaction
     if valid?
+      @sender -= amount
+      @receiver += amount
       status = 'complete'
     else
       false
